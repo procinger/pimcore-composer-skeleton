c6f419e4fc804f217b9418cdf5e2496cc7a7d2c2
procinger <proc@go4more.de>
2016-12-11 11:38:13 +0100

    init commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Installing
+Create database
+```
+user@host$ mysql -u root -p -e "CREATE DATABASE pimcore charset=utf8mb4;"
+user@host$ git clone https://github.com/procinger/pimcore-composer-skeleton.git /path/to/document/root
+user@host$ cd /path/to/document/root
+user@host$ composer create-project
+```
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - GNU General Public License version 3 (GPLv3)
+* - Pimcore Enterprise License (PEL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+* @copyright  Copyright (c) 2009-2016 pimcore GmbH (http://www.pimcore.org)
+* @license    http://www.pimcore.org/license     GPLv3 and PEL
+*/
+if (!defined("PIMCORE_DOCUMENT_ROOT")) {
+    define("PIMCORE_DOCUMENT_ROOT", __DIR__);
+}
+
+include_once('./vendor/pimcore/pimcore/index.php');
